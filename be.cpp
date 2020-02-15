923e327414751ed1534e5bc35ef03f1b2b5c8305
ya0201 <you@example.com>
2020-02-15 17:46:52 +0900

    Refactor

--- be.cpp
+++ be.cpp
@@ -5,7 +5,7 @@
  *                  (mtoyoda@acm.org)
  *        Last Modified: 2014/06/03
  *
- *	Modified by ya0201: 2019/12/22
+ *	Modified to be.cpp by ya0201: 2019/12/22~
  *========================================
  */
 /* be version 1.00 : Initial build executioner.                              */
@@ -43,18 +43,15 @@
 /*                                              by Toyoda Masashi 1992/12/11 */
 
 #include <curses.h>
-#include <signal.h>
-#include <unistd.h>
-#include <stdio.h>
-#include <iostream>
 #include <fstream>
+#include <iostream>
+#include <signal.h>
+#include <stdio.h>
 #include <string>
+#include <unistd.h>
 #include <OpenAL/al.h>
 #include <OpenAL/alc.h>
 #include "be.h"
-
-// int add_BE();
-// int my_mvaddstr(int y, int x, char *str);
 
 int my_mvaddstr(int y, int x, char *str) {
     for ( ; x < 0; ++x, ++str)
@@ -122,15 +119,15 @@ int add_BE() {
 
 
 //check big vs little endian machine
-static bool IsBigEndian(void) {
+static bool is_big_endian(void) {
   int a = 1;
   return !((char*)&a)[0];
 }
 
-static int ConvertToInt(char* buffer, int len) {
+static int convert_to_int(char* buffer, int len) {
   int a = 0;
 
-  if(!IsBigEndian()) {
+  if(!is_big_endian()) {
     for(int i = 0; i < len; ++i) {
       ((char*)&a)[i] = buffer[i];
     }
@@ -144,7 +141,7 @@ static int ConvertToInt(char* buffer, int len) {
 }
 
 //Location and size of data is found here: http://www.topherlee.com/software/pcm-tut-wavformat.html
-static char* LoadWAV(std::string filename, int& channels, int& sampleRate, int& bps, int& size) {
+static char* load_wav(std::string filename, int& channels, int& sampleRate, int& bps, int& size) {
   char buffer[4];
 
   std::ifstream in(filename.c_str());
@@ -173,26 +170,26 @@ static char* LoadWAV(std::string filename, int& channels, int& sampleRate, int& 
 
   in.read(buffer, 4);//Length of format data. Should be 16 for PCM, meaning uncompressed.
 
-  if(ConvertToInt(buffer, 4) != 16) {
+  if(convert_to_int(buffer, 4) != 16) {
     std::cout << "Error here, not a valid WAV file, format length wrong in header.\n This was found instead: "
-              << ConvertToInt(buffer, 4) << std::endl;
+              << convert_to_int(buffer, 4) << std::endl;
   }
 
   in.read(buffer, 2);//Type of format, 1 = PCM
 
-  if(ConvertToInt(buffer, 2) != 1) {
+  if(convert_to_int(buffer, 2) != 1) {
     std::cout << "Error here, not a valid WAV file, file not in PCM format.\n This was found instead: "
-              << ConvertToInt(buffer, 4) << std::endl;
+              << convert_to_int(buffer, 4) << std::endl;
   }
 
   in.read(buffer, 2);//Get number of channels. 
 
   //Assume at this point that we are dealing with a WAV file. This value is needed by OpenAL
-  channels = ConvertToInt(buffer, 2);
+  channels = convert_to_int(buffer, 2);
 
   in.read(buffer, 4);//Get sampler rate. 
 
-  sampleRate = ConvertToInt(buffer, 4);
+  sampleRate = convert_to_int(buffer, 4);
 
   //Skip Byte Rate and Block Align. Maybe use later?
   in.read(buffer, 4);//Block Align
@@ -200,14 +197,14 @@ static char* LoadWAV(std::string filename, int& channels, int& sampleRate, int& 
 
   in.read(buffer, 2);//Get Bits Per Sample
 
-  bps = ConvertToInt(buffer, 2);
+  bps = convert_to_int(buffer, 2);
 
   //Skip character data, which marks the start of the data that we care about. 
   in.read(buffer, 4);//"data" chunk. 
 
   in.read(buffer, 4); //Get size of the data
 
-  size = ConvertToInt(buffer, 4);
+  size = convert_to_int(buffer, 4);
 
   if(size < 0) {
     std::cout << "Error here, not a valid WAV file, size of file reports 0.\n This was found instead: "
@@ -254,7 +251,7 @@ int main(int argc, char *argv[]) {
   attrset(COLOR_PAIR(1));
 
   // initialize OpenAL
-  data = LoadWAV("data/shinobi_execution_se.wav", channel, sample_rate, bps, size);
+  data = load_wav("data/shinobi_execution_se.wav", channel, sample_rate, bps, size);
   device = alcOpenDevice(nullptr);
   if (!device) {
     std::cout << "Could not open sound device" << std::endl;
